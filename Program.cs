dcf2108f2524989cf425f4eb7721866836700661
yukio-sato <112220117+yukio-sato@users.noreply.github.com>
2023-04-01 19:05:26 -0300

    [01/04/2023] Atividade de Troco

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿decimal valor, preco;
+void frase(string frase)
+{
+    for (int i = 0; i < frase.Length; i++)
+    Console.Write(frase[i]);
+    Thread.Sleep(75);
+{
+    
+}
+}
+frase("Quantia de dinheiro: ");
+valor = decimal.Parse(Console.ReadLine()!);
+frase("Preço do produto: ");
+preco = decimal.Parse(Console.ReadLine()!);
+if (valor-preco > 0)
+{
+frase($"Seu troco foi de {valor-preco} da compra.");
+}
+else
+{
+    frase($"Você deve {-(valor-preco)} para a compra.");
+}
